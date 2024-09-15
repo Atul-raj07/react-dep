4b6964cf5cd11afae0faf6f6dbc278eaa22d402d
Atul-raj07 <atulrajgupta12j@gmail.com>
2024-09-15 17:51:18 +0530

    changed some text

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,10 @@ let a = "5";
 const App = () => {
   return (
     <>
-      <h1 className='text-5xl text-red-500'>dfjfdkj</h1>
+      <h1 className='text-5xl text-red-500'>This is a react App testing</h1>
       <br />
       <h2> {import.meta.env.VITE_API_KEY }</h2>
+      
     </>
   )
 }
