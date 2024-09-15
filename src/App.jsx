0dc2115cca39e99472c780702bad458c592aff32
Atul-raj07 <atulrajgupta12j@gmail.com>
2024-09-15 17:32:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+let a = "5";
+
+const App = () => {
+  return (
+    <>
+      <h1 className='text-5xl text-red-500'>dfjfdkj</h1>
+      <br />
+      <h2> {import.meta.env.VITE_API_KEY }</h2>
+    </>
+  )
+}
+
+export default App
