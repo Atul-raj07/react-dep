8c8eed9015d1db97c89bd994c753c217fe651242
Atul-raj07 <atulrajgupta12j@gmail.com>
2024-09-24 21:06:28 +0530

    practice of useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react'
 
-let b = [
-  {
-    name: 'a',
-    age: 10,
-    gender: 'male',
-  },
-  {
-    name: 'b',
-    age: 20,
-    gender: 'female',
-  },
-  {
-    name: 'c',
-    age: 30,
-    gender: 'male',
-  },
-];
-
-let data = b.map((item,i) =>{
-  return (
-    <div key={i}>
-  <h1 className='text-4xl'>{item.name}</h1>
-  <h4>{item.age}</h4>
-  <p>{item.gender}</p>
-  </div>
-  )
-} );
-console.log(data);
 const App = () => {
-  return (
-    <>
-      <div>{data}</div>
-    </>
-  );
-};
 
-export default App;
+  const [first,setfirst] = useState("jvhb")
+  console.log(first)
+  
+  return (
+    <div>
+      <h1>{first}</h1>
+      <button onClick={()=>{setfirst("hello")}} className='px-4 py-1 bg-red-600 m-5' >click</button>
+    </div>
+  )
+}
+
+export default App
